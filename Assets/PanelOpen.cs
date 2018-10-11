a759c18c37c3161419c92d350e9f43235bb7ffc8
kabirules <javifont@gmail.com>
2018-10-12 00:02:57 +0200

    File selection

---
+++ Assets/PanelOpen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class PanelOpen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		string path = EditorUtility.OpenFilePanel("Select a music file", "", "*");
+		Debug.Log(path);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
