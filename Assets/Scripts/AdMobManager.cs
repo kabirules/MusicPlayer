619f17e58879fe48a872f96920edab5b81808eff
kabirules <javifont@gmail.com>
2018-10-22 23:20:17 +0200

    AdMob SDK

---
+++ Assets/Scripts/AdMobManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class AdMobManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        #if UNITY_ANDROID
+            string appId = "ca-app-pub-3940256099942544~3347511713";
+        #elif UNITY_IPHONE
+            string appId = "ca-app-pub-3940256099942544~1458002511";
+        #else
+            string appId = "unexpected_platform";
+        #endif
+
+        // Initialize the Google Mobile Ads SDK.
+        MobileAds.Initialize(appId);		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
